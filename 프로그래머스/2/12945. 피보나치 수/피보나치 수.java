aeaf4fb6b4d8f091f87fc0f5ed2f10345d464a5c
임세희 <109964874+Sehi55@users.noreply.github.com>
2026-02-12 10:41:50 +0900

    [level 2] Title: 피보나치 수, Time: 2.78 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -3,7 +3,7 @@ class Solution {
         int[] list = new int[n+1];
         list[0]=0; list[1]=1;
         for(int i =2;i<n+1;i++){
-            list[i]=(list[i-2]%1234567+list[i-1]%1234567)%1234567;
+            list[i]=(list[i-2]+list[i-1])%1234567;
         }
         return list[n];
     }
